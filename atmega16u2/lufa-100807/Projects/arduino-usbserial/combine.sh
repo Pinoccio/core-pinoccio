4f2ec7a30c483e6d68a8bfe0a6974c8d6d264c0a
Eric Jennings <ericj@loopshot.com>
2012-11-06 19:29:01 -0800

    Create combined 8U2 hex file using gcc 4.7.2 to solve buggy earlier issues

--- atmega16u2/lufa-100807/Projects/arduino-usbserial/combine.sh
+++ atmega16u2/lufa-100807/Projects/arduino-usbserial/combine.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-(sed '$d' Pinoccio-usbserial.hex; cat Pinoccio-usbdfu.hex) > Pinoccio-Combined.hex
+(sed '$d' Pinoccio-usbserial-4.7.2.hex; cat Pinoccio-usbdfu-4.7.2.hex) > Pinoccio-Combined-4.7.2.hex
