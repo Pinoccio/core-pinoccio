b169121b9a566de074f7a31f4e392b301f601648
Eric Jennings <ericj@loopshot.com>
2013-10-04 13:03:59 -0700

    Added some libraries to avr pinoccio core

--- hardware/pinoccio/avr/bootloaders/pinoccio/combine.sh
+++ hardware/pinoccio/avr/bootloaders/pinoccio/combine.sh
@@ -3,4 +3,4 @@
 (sed '$d' EdDemo.hex; cat ATmegaBOOT_atmega128rfa1.hex) > combined.hex
 
 echo "Install combined hex with the following avrdude command:"
-echo "avrdude -p m128rfa1 -P usb -c avrisp2 -U flash:w:ATmegaBOOT_atmega128rfa1.hex:m -U lfuse:w:0xF7:m -U hfuse:w:0xD0:m -U efuse:w:0xF8:m"
+echo "avrdude -p m128rfa1 -P usb -c avrisp2 -U flash:w:ATmegaBOOT_atmega128rfa1.hex:m -U lfuse:w:0xF7:m -U hfuse:w:0xD0:m -U efuse:w:0xFE:m"
